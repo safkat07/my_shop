bd4bd62de0de26cbc2af767085ecd2fa5e2739c7
safkat07 <safkatsadli@gmail.com>
2023-08-20 23:33:54 +0600

    done

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function clickCard(target) {
   sumOfTotalPrice = parseFloat(sumOfTotalPrice) + parseFloat(cardPrice);
   console.log(parseFloat(sumOfTotalPrice));
   const TotalPrice = document.getElementById("total-price");
-  TotalPrice.innerText = sumOfTotalPrice;
+  TotalPrice.innerText = sumOfTotalPrice.toFixed(2);
   const couponButton = document.getElementById("cuopon-btn");
   if (sumOfTotalPrice >= 200) {
     couponButton.removeAttribute("disabled");
@@ -27,3 +27,31 @@ function clickCard(target) {
     couponButton.setAttribute("disabled");
   }
 }
+
+//coupon button
+document.getElementById("cuopon-btn").addEventListener("click", function () {
+  const couponInputFiled = document.getElementById("coupon-input-field");
+  const couponInputFiledValue = couponInputFiled.value;
+
+  if (couponInputFiledValue === "" || couponInputFiledValue === " ") {
+    alert("enter value");
+    return;
+  }
+
+  if (couponInputFiledValue === "SELL200") {
+    // console.log("you got dicsount");
+    const dicsountPrice = document.getElementById("discount-price");
+    const sumOfTotalDiscountPrice = sumOfTotalPrice * 0.2;
+
+    dicsountPrice.innerText = sumOfTotalDiscountPrice.toFixed(2);
+
+    const finalTotalPrice = document.getElementById("final-total-price");
+    finalTotalPrice.innerText = (
+      sumOfTotalPrice - sumOfTotalDiscountPrice
+    ).toFixed(2);
+  }
+  else{
+    alert("Enter The Right Code")
+    couponInputFiled.value = ""
+  }
+});
